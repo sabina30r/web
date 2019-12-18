28c950b8d5e21885c99f78cbb16e489e42ab49af
sabina30r <47630521+sabina30r@users.noreply.github.com>
2019-12-18 03:13:44 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php include_once "tags/page-meta.php" ?>
+<body>
+<!-- navbar -->
+<?php include_once "tags/home-tag.php"; ?>
+
+<!-- header -->
+<header class="header ">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="description ">
+            <h1>
+                Hello , welcome to my website!
+                <p>
+                    This site was created by Tatiana Kovalyova.
+                </p>
+            </h1>
+        </div>
+    </div>
+</header>
+
+<div class="contact-form" id="contact">
+    <div class="container">
+
+    </div>
+</div>
+<!-- add Javasscript file from js file -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<script type="text/javascript" src="js/main.js"></script>
+</body>
+</html>
